fe99cac2f92190953bb932b1a3d9c3ac362eedd7
John O'Dowd <114420852@umail.ucc.ie>
2018-06-10 02:50:29 +0100

    working! added epoch

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+var start = Date.now();
+
 var x = null;
 var y = null;
 
 xval = document.getElementById("xval");
 yval = document.getElementById("yval");
+randval = document.getElementById("randval");
+
     
 document.addEventListener('mousemove', onMouseUpdate, false);
 document.addEventListener('mouseenter', onMouseUpdate, false);
@@ -13,12 +17,26 @@ function onMouseUpdate(e) {
 
   xval.innerHTML = x;
   yval.innerHTML = y;
+  randval.innerHTML = convertToRange(getRandomNumberWithMouseCoords(x , y));
   
 }
 
-function getRandomNumber() {
-	num = Math.floor((Math.random() * 100) + 1);
-	return num;
+function getRandomNumberWithMouseCoords(x, y) {
+	
+  //num = Math.floor((Math.random() * 100) + 1);
+	Math.seedrandom((x+1) * (y+1) + getCurrentTime());
+  num = Math.random();
+  return num;
+}
+
+function getCurrentTime(){
+  var millis = Date.now() - start;
+  return millis;
+}
+
+function convertToRange(n){
+  num = Math.floor(n * 100) + 1;
+  return num;
 }
 
 function getMouseX() {
